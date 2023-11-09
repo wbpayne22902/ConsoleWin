8a3ced79be554e222969d722c52658b2635044d5
Wilhelm Payne <wbpayne@wilhelmpayne.com>
2023-11-08 22:01:39 -0500

    Added colors
    Added colorsto output

--- ConsoleWin/ConsoleWin.cpp
+++ ConsoleWin/ConsoleWin.cpp
@@ -4,10 +4,13 @@ using namespace System;
 
 int main(array<System::String ^> ^args)
 {
+    Console::BackgroundColor = ConsoleColor::Blue;
+    Console::ForegroundColor = ConsoleColor::DarkRed;
     Console::WriteLine("Hello, World");
     ShellAboutW(NULL, L"My App", L"Cool", NULL);
     Beep(550, 500);
     printf("About Window Closed\n");
     Console::WriteLine("That's all folks!");
+    Console::ResetColor();
     return 0;
 }
