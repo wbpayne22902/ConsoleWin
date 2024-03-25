7692f5f1b8f29bc0024b03c2a5941342ed0cf0f4
Wilhelm Payne <wvonhimmel@outlook.com>
2024-03-25 08:32:49 -0400

    Updated
    This is cool!

--- ConsoleWin/ConsoleWin.cpp
+++ ConsoleWin/ConsoleWin.cpp
@@ -7,7 +7,7 @@ int main(array<System::String ^> ^args)
     Console::BackgroundColor = ConsoleColor::Blue;
     Console::ForegroundColor = ConsoleColor::DarkRed;
     Console::WriteLine("Hello, World");
-    ShellAboutW(NULL, L"My App", L"Cool", NULL);
+    ShellAboutW(NULL, L"My App#Wow Nice!", L"Cool", NULL);
     Beep(550, 500);
     printf("About Window Closed\n");
     Console::WriteLine("That's all folks!");
